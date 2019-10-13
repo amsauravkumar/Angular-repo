641bee8410f79bfa7a68d73a664b76e6723d99fc
Saurav <amsauravkumar@gmail.com>
2019-10-13 19:03:00 +0530

    retriving query params

--- src/app/modules/login/login-page/login-page.component.ts
+++ src/app/modules/login/login-page/login-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 
+
+
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
@@ -8,25 +10,35 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
+  allowEdit = false;
+
   user: {
     id: number;
   }
   constructor(private activeRoute: ActivatedRoute, private _route: Router) { }
 
   ngOnInit() {
+    console.log(this.activeRoute.snapshot.params['id'])
     this.user = {
       id: this.activeRoute.snapshot.params['id']
     }
+    console.log(this.activeRoute.snapshot.queryParams.allowLoading)
     this.activeRoute.params
       .subscribe((params: Params) => {
         this.user.id = params['id']
+      })
+
+      this.activeRoute.queryParams
+      .subscribe((params: Params) => {
+        this.allowEdit = +  params['allowLoading'] === 1 ? true:false;
+        console.log(this.allowEdit)
       })
 
   }
 
 
   routeFunction(id : number) {
-    this._route.navigate(['login', id, 'edit'], {queryParams: {allowLoading :1} , fragment: 'loading'});
+    this._route.navigate(['login', id, 'edit'], {queryParams: {allowLoading: id === 123? 1:0} , fragment: 'loading'});
   }
 
-} 
+}   
